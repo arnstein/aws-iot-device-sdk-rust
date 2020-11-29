4ae63591ebb1ed58e68cf9f21552d0c7a557082c
Yuma Mihira <yuma-m@users.noreply.github.com>
2020-11-29 21:41:53 +0900

    Update subscribe and add unsubscribe method

--- src/client.rs
+++ src/client.rs
@@ -1,22 +1,29 @@
-use std::{fs::read, thread, collections::HashMap, sync::{Arc, Mutex}};
-use rumqtt::{QoS, ConnectError, ReconnectOptions, Receiver, Notification, MqttClient, MqttOptions};
+use rumqtt::{
+    ConnectError, MqttClient, MqttOptions, Notification, QoS, Receiver, ReconnectOptions,
+};
+use std::{
+    collections::HashMap,
+    fs::read,
+    sync::{Arc, Mutex},
+    thread,
+};
 
 pub struct AWSIoTClient {
     pub aws_iot_client: MqttClient,
     receiver: Receiver<Notification>,
     callback_map: Arc<Mutex<HashMap<String, fn(String)>>>,
+    is_listening: bool,
 }
 
 impl AWSIoTClient {
-
     /// Returns an AWSIoTClient struct with the instantiated MQTT client ready to be used.
     pub fn new(
         client_id: &str,
         ca_path: &str,
         client_cert_path: &str,
         client_key_path: &str,
-        aws_iot_endpoint: &str) -> Result<Self, ConnectError> {
-
+        aws_iot_endpoint: &str,
+    ) -> Result<Self, ConnectError> {
         let mqtt_options = MqttOptions::new(client_id, aws_iot_endpoint, 8883)
             .set_ca(read(ca_path)?)
             .set_client_auth(read(client_cert_path)?, read(client_key_path)?)
@@ -26,8 +33,9 @@ impl AWSIoTClient {
         Ok(AWSIoTClient {
             aws_iot_client: mqtt_client.0,
             receiver: mqtt_client.1,
-            callback_map: Arc::new(Mutex::new(HashMap::new()))
-            })
+            callback_map: Arc::new(Mutex::new(HashMap::new())),
+            is_listening: false,
+        })
     }
 
     /// Associates a callback function with a topic name.
@@ -40,10 +48,7 @@ impl AWSIoTClient {
 
     /// Remove the callback function associated with a topic name.
     pub fn remove_callback(&self, topic_name: String) -> Option<fn(String)> {
-        self.callback_map
-            .lock()
-            .unwrap()
-            .remove(&topic_name)
+        self.callback_map.lock().unwrap().remove(&topic_name)
     }
 
     /// When called it will spawn a thread that checks if the HashMap in the AWSIoTClient contain
@@ -52,32 +57,42 @@ impl AWSIoTClient {
         let callback_map = Arc::clone(&self.callback_map);
         let receiver = self.receiver.clone();
         thread::spawn(move || loop {
-                for notification in &receiver {
-                    match notification {
-                        rumqtt::client::Notification::Publish(packet) => {
-                            match callback_map
-                                .lock()
-                                .unwrap()
-                                .get(&packet.topic_name) {
-                                    Some(&func) => {
-                                    func(String::from_utf8(packet.payload.to_vec()).unwrap());
-                                }
-                                _ => (),
+            for notification in &receiver {
+                match notification {
+                    rumqtt::client::Notification::Publish(packet) => {
+                        match callback_map.lock().unwrap().get(&packet.topic_name) {
+                            Some(&func) => {
+                                func(String::from_utf8(packet.payload.to_vec()).unwrap());
                             }
-                        },
-                        _ => (),
+                            _ => (),
+                        }
                     }
+                    _ => (),
                 }
+            }
         });
     }
 
     /// Subscribe to any topic.
-    pub fn subscribe (&mut self, topic_name: String, qos: QoS) {
-            self.aws_iot_client.subscribe(topic_name, qos).unwrap();
+    pub fn subscribe(&mut self, topic_name: String, qos: QoS, callback: fn(String)) {
+        self.aws_iot_client.subscribe(&topic_name, qos).unwrap();
+        if !self.is_listening {
+            self.add_callback(topic_name, callback);
+            self.start_listening();
+            self.is_listening = true;
+        }
+    }
+
+    // Subscribe from a topic.
+    pub fn unsubscribe(&mut self, topic_name: String) {
+        self.aws_iot_client.unsubscribe(&topic_name).unwrap();
+        self.remove_callback(topic_name);
     }
 
     /// Publish to any topic.
-    pub fn publish (&mut self, topic_name: String, qos: QoS, payload: &str) {
-            self.aws_iot_client.publish(topic_name, qos, false, payload).unwrap();
+    pub fn publish(&mut self, topic_name: String, qos: QoS, payload: &str) {
+        self.aws_iot_client
+            .publish(topic_name, qos, false, payload)
+            .unwrap();
     }
 }
--- src/main_pubsub.rs
+++ src/main_pubsub.rs
@@ -17,12 +17,12 @@ fn main() {
     let iot_client =
         &mut client::AWSIoTClient::new(CLIENT_ID, CA_CERT, CLIENT_CERT, PRIVATE_KEY, IOT_ENDPOINT)
             .unwrap();
+    println!("Connect: {}", IOT_ENDPOINT);
 
     const TOPIC: &str = "data/test";
 
-    iot_client.subscribe(TOPIC.to_string(), QoS::AtMostOnce);
-    iot_client.add_callback(TOPIC.to_string(), callback);
-    iot_client.start_listening();
+    iot_client.subscribe(TOPIC.to_string(), QoS::AtMostOnce, callback);
+    thread::sleep(time::Duration::from_secs(1));
 
     for i in 0..10 {
         let payload = format!("{{\"test\": \"Hello world {}.\"}}", i);
