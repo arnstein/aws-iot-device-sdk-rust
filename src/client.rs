fd7d1d896281424cda9c428206be05195068aa15
Arnstein Kleven <arnsteinkleven@gmail.com>
2021-02-26 13:19:29 +0100

    wip

--- src/client.rs
+++ src/client.rs
@@ -1,16 +1,23 @@
 use std::fs::read;
-use rumqttc::{self, Event, Packet, Incoming, Client, LastWill, Connection, MqttOptions, Publish, PubAck, SubAck, UnsubAck, QoS, ConnectionError};
+use std::sync::mpsc::{channel, Receiver, Sender};
+use rumqttc::{self, Event, Incoming, Client, LastWill, Connection, MqttOptions, Publish, QoS, ConnectionError};
+use rumqttc::{Sender as RumqttcSender, Connect, PubAck, SubAck, UnsubAck, PubRec, PubRel, PubComp, Subscribe, Unsubscribe, Disconnect, ConnAck, Request, ClientError};
 
 #[cfg(feature= "async")]
-use rumqttc::{EventLoop, AsyncClient, Subscribe, Request};
-use async_channel::Sender;
+use rumqttc::{EventLoop, AsyncClient};
 
 pub trait AWSEventHandler {
 
-    fn on_connect(&self) {}
-    fn on_connack(&self) {}
-    fn on_publish(&self, message: Publish) {}
+    fn on_connect(&mut self, connect: Connect) {}
+    fn on_connack(&mut self, connect_info: ConnAck) {}
+    fn on_publish(&mut self, message: Publish) {}
     fn on_puback(&self, puback: PubAck) {}
+    fn on_pubrec(&self, pubrec: PubRec) {}
+    fn on_pubrel(&self, pubrel: PubRel) {}
+    fn on_pubcomp(&self, pubcomp: PubComp) {}
+    fn on_subscribe(&self, subscribe: Subscribe) {}
+    fn on_unsubscribe(&self, unsubscribe: Unsubscribe) {}
+    fn on_disconnect(&self) {}
     fn on_suback(&self, suback: SubAck) {}
     fn on_unsuback(&self, unsuback: UnsubAck) {}
     fn on_pingreq(&self) {}
@@ -18,13 +25,12 @@ pub trait AWSEventHandler {
 
 }
 
-//fn start_event_listener(mut connection: Connection, aws_struct: Box<dyn AWSEventHandler>) {
-fn start_event_listener<T: AWSEventHandler + ?Sized> (mut connection: Connection, aws_struct: &T) {
-    for notification in connection.iter() {
-        match notification.unwrap() {
+fn incoming_event_handler<T: AWSEventHandler + ?Sized> (receiver: Receiver<Event>, aws_struct: &mut T) {
+    let message = receiver.recv().unwrap(); 
+        match message {
             Event::Incoming(i) => {
                 match i {
-                    Incoming::Connect(c) => aws_struct.on_connect(),
+                    Incoming::Connect(connect) => aws_struct.on_connect(connect),
                     Incoming::Publish(message) => {
                         aws_struct.on_publish(message);
                     },
@@ -44,52 +50,31 @@ fn start_event_listener<T: AWSEventHandler + ?Sized> (mut connection: Connection
                         aws_struct.on_pingresp();
                     },
                     Incoming::ConnAck(c) => {
-                        aws_struct.on_connack();
+                        aws_struct.on_connack(c);
                     },
-                    _ => (),
+                    Incoming::PubRec(pubrec) => {
+                        aws_struct.on_pubrec(pubrec);
+                    },
+                    Incoming::PubRel(pubrel) => {
+                        aws_struct.on_pubrel(pubrel);
+                    },
+                    Incoming::PubComp(pubcomp) => {
+                        aws_struct.on_pubcomp(pubcomp);
+                    },
+                    Incoming::Subscribe(subscribe) => {
+                        aws_struct.on_subscribe(subscribe);
+                    },
+                    Incoming::Unsubscribe(unsubscribe) => {
+                        aws_struct.on_unsubscribe(unsubscribe);
+                    },
+                    Incoming::Disconnect => {
+                        aws_struct.on_disconnect();
+                    },
                 }
             },
             _ => (),
         }
     }
-}
-
-#[cfg(feature= "async")]
-pub async fn start_async_event_listener<T: AWSEventHandler + ?Sized> (mut eventloop: EventLoop, aws_struct: &T) -> Result<(), ConnectionError>{
-
-    loop {
-        match eventloop.poll().await? {
-            Event::Incoming(i) => {
-                match i {
-                    Incoming::Connect(c) => aws_struct.on_connect(),
-                    Incoming::Publish(message) => {
-                        aws_struct.on_publish(message);
-                    },
-                    Incoming::PubAck(puback) => {
-                        aws_struct.on_puback(puback);
-                    },
-                    Incoming::SubAck(suback) => {
-                        aws_struct.on_suback(suback);
-                    },
-                    Incoming::UnsubAck(unsuback) => {
-                        aws_struct.on_unsuback(unsuback);
-                    },
-                    Incoming::PingReq => {
-                        aws_struct.on_pingreq();
-                    },
-                    Incoming::PingResp => {
-                        aws_struct.on_pingresp();
-                    },
-                    Incoming::ConnAck(c) => {
-                        aws_struct.on_connack();
-                    },
-                    _ => (),
-                }
-            },
-            _ => (),
-        }
-    }
-}
 
 pub struct AWSIoTSettings {
         client_id: String,
@@ -153,10 +138,20 @@ impl AWSIoTClient {
 pub struct AWSIoTAsyncClient {
     pub client: AsyncClient,
     pub eventloop: EventLoop,
+    pub incoming_event_sender: Sender<Incoming>,
+    pub incoming_event_receiver: Receiver<Incoming>,
+}
+
+pub async fn aws_subscribe<S: Into<String>>(request_tx: RumqttcSender<Request>, topic: S, qos: QoS) -> Result<(), ClientError> {
+    let subscribe = Subscribe::new(topic.into(), qos);
+    let request = Request::Subscribe(subscribe);
+    request_tx.send(request).await.unwrap();
+    Ok(())
 }
 
 #[cfg(feature= "async")]
 impl AWSIoTAsyncClient {
+
 
     pub async fn new(
         client_id: &str,
@@ -179,16 +174,19 @@ impl AWSIoTAsyncClient {
         }
 
         let (client, eventloop) = AsyncClient::new(mqtt_options, 10);
-        Ok(AWSIoTAsyncClient { client, eventloop })
+        let (request_tx, request_rx) = channel();
+        Ok(AWSIoTAsyncClient { client, eventloop, incoming_event_receiver: request_rx, incoming_event_sender: request_tx })
     }
 
-    /// Subscribe to any topic.
-    pub async fn subscribe (&mut self, topic_name: String, qos: QoS) {
-        self.client.subscribe(topic_name, qos).await.unwrap();
-    }
-
-    /// Publish to any topic.
-    pub async fn publish (&mut self, topic_name: String, qos: QoS, payload: &str) {
-        self.client.publish(topic_name, qos, false, payload).await.unwrap();
+    pub async fn listen(&mut self) -> Result<(), ConnectionError>{
+        loop {
+            match self.eventloop.poll().await? {
+                Event::Incoming(i) => {
+                    self.incoming_event_sender.send(i);
+                },
+                _ => (),
+                // => println!("Got: {:?}"),
+            }
+        }
     }
 }
--- src/shadow.rs
+++ src/shadow.rs
@@ -4,35 +4,5 @@ use rumqttc::{self, Incoming, Client, Connection, MqttOptions, Publish, PubAck, 
 
 
 pub trait AWSShadow {
-    const ID: String;
-
-    fn on_shadow_update(shadow: Value) {}
-    fn on_shadow_get(shadow: Value) {}
-    fn get_shadow() {}
-
-    fn start_shadow_listener(&self, mut connection: Connection) {
-        let shadow_get_topic = format!("$aws/things/{}/shadow/get/accepted", Self::ID).to_string();
-        let shadow_update_topic = format!("$aws/things/{}/shadow/update/accepted", Self::ID).to_string();
-        for notification in connection.iter() {
-            match notification {
-                Ok(notification_type) => match notification_type.0 {
-                    Some(Incoming::Publish(message)) => {
-                        match message.topic {
-                            shadow_update_topic => {
-                                let shadow: Value = serde_json::from_str(str::from_utf8(&message.payload).unwrap()).unwrap();
-                                Self::on_shadow_update(shadow)
-                            },
-                            shadow_get_topic => {
-                                let shadow: Value = serde_json::from_str(str::from_utf8(&message.payload).unwrap()).unwrap();
-                                Self::on_shadow_get(shadow)
-                            },
-                            _ => (),
-                        }
-                    },
-                    _ => (),
-                },
-                Err(_) => (),
-            }
-        }
-    }
+    item: Value,
 }
